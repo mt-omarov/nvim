4e6eb932a8cc2a744b40bea65e836c3556d84c73
Marat Omarov <mtomarov@miem.hse.ru>
2024-11-04 15:06:38 +0300

    Add tmux manager & keymaps, hide cowboy & bad keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Deprecate moving spam for forcing to use counts:
+-- local discipline = require("kitten.discipline")
+-- discipline.cowboy()
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+
+-- Tmux navigation
+keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
@@ -22,22 +33,7 @@ keymap.set("n", "te", ":tabedit", opts)
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", "tabprev<Return>", opts)
 
--- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
-
--- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
-
 -- Resize window
 keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
-
--- Diagnostics
-keymap.set("n", "<C-j>", function()
-  vim.diagnostic.goto_next()
-end, opts)
